a9158b73012269fc3dc72e0aa7d695f3b2c59d34
Unknown <matt.langley@system1group.com>
2019-08-18 20:12:54 +0100

    some logic to help visualise the flow of obtaining tasks from the api

--- Todo.Api/Todo.Api/Services/ITaskFactory.cs
+++ Todo.Api/Todo.Api/Services/ITaskFactory.cs
@@ -1,9 +1,10 @@
 ﻿namespace Todo.Api.Services
 {
+    using System.Collections.Generic;
     using Models;
 
     public interface ITaskFactory
     {
-        Task Create(Models.Database.Task task);
+        IEnumerable<Task> Create(IEnumerable<Models.Database.Task> task);
     }
 }
